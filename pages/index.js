faf24cccc572e00d11c7822989f4714466ee53e2
Ranipandit <rani17@navgurukul.org>
2019-03-13 22:49:22 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../src/components/Header';
+
+class Index extends React.Component {
+    render(){
+        return(
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Index
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+} from "reactstrap";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar color="green" light>
+          <NavbarBrand href="/" className="mr-auto">
+            reactstrap
+          </NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
